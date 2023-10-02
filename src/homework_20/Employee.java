50b557c9d9ccb0d291fcb96a7249819b4187dcdb
TheKirrrra <kirrrra007@gmail.com>
2023-10-02 21:34:27 +0200

    update homework 20

--- src/homework_20/Employee.java
+++ src/homework_20/Employee.java
@@ -8,9 +8,9 @@ public class Employee {
 
     private String name = "Incognito";
 
-    private int age = 18; // минимальный возраст для работы
+    private int age = 18; // минимальный возраст для работы.
 
-    private int salary = 450; // минимальная зарплата 450
+    private int salary = 450; // минимальная зарплата 450.
 
     public Employee(String name, int age, int salary) {
         setName(name);
