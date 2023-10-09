ea63ca68e271c79a04c8acc10419db32a9572391
TheKirrrra <kirrrra007@gmail.com>
2023-10-09 15:10:19 +0200

    homework 21

---
+++ src/homework_22/binear.java
@@ -0,0 +1,21 @@
+package homework_22;
+
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+import static homework_22.main.*;
+
+public class binear {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int[] ints = new int [1000];
+        addRandomValues(ints);
+        sortArray(ints);
+        System.out.println("Какое число хотите найти?");
+        int searchMe = scanner.nextInt();
+        binearSearch(ints, searchMe);
+        System.out.println(Arrays.toString(ints));
+    }
+}
