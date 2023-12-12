c2f002748af50d54b1ac3cebd0bc2451ae588fdc
Kirrrra <kirrrra007@gmail.com>
2023-12-12 18:10:13 +0100

    lesson 64 + homework 64

--- src/homeworks/homework49_/homework_63/task3.java
+++ src/homeworks/homework49_/homework_63/task3.java
@@ -7,7 +7,7 @@ public class task3 {
     public static void main(String[] args) {
         String task = "Мои номера: +123-456-7890, +987-654-3210, +1234-123-1234, +234-2345-2345, +345-345-345.";
 
-        Pattern pattern = Pattern.compile("\\b+\\d{3}-\\d{3}-\\d{4}");
+        Pattern pattern = Pattern.compile("\\+\\d{3}-\\d{3}-\\d{4}");
 
         Matcher matcher = pattern.matcher(task);
         while (matcher.find()) {
