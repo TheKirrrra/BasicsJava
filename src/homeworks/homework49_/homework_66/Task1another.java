d4dcad04e8c3a375dc87f7ca6e109cb4359073f8
Kirrrra <kirrrra007@gmail.com>
2023-12-18 15:56:51 +0100

    lesson 66 + homework 66

---
+++ src/homeworks/homework49_/homework_66/Task1another.java
@@ -0,0 +1,32 @@
+package homeworks.homework49_.homework_66;
+
+public class Task1another {
+    private static int counter = 0;
+
+    public static void main(String[] args) {
+        Object lock = new Object();
+
+        Thread increaseThread = new Thread(() -> {
+            for (int i = 0; i < 1000; i++) {
+                synchronized (lock) {
+                    counter++;
+                }
+            }
+        });
+
+        Thread decreaseThread = new Thread(() -> {
+            for (int i = 0; i < 993; i++) {
+                synchronized (lock) {
+                    counter--;
+                }
+            }
+        });
+
+        increaseThread.start();
+        decreaseThread.start();
+
+        System.out.println("Final counter value: " + counter);
+    }
+}
+
+
